4e5ac6a3d38b73487724c93c259835b1e977d8b9
Rohit Bhatia <ROHIT.BHATIA@VIZIENTINC.COM>
2020-07-20 15:26:10 -0500

    String Segmentation

--- src/other_algo/string/StringSegmentation.java
+++ src/other_algo/string/StringSegmentation.java
@@ -1,5 +1,7 @@
 package other_algo.string;
 
+import java.util.HashSet;
+
 /*
     You are given a dictionary of words and a large input string. You have to find out whether the input string
     can be completely segmented into the words of a given dictionary. The following two examples elaborate on the
@@ -8,5 +10,32 @@ package other_algo.string;
 https://www.educative.io/m/string-segmentation
  */
 public class StringSegmentation {
+
+    public static void main(String[] argu) {
+        String lookup = "applepie";
+        HashSet<String> dictionary = new HashSet<>();
+        dictionary.add("apple");
+        dictionary.add("pear");
+        dictionary.add("pier");
+        dictionary.add("pie");
+
+        System.out.println(segmentedString(lookup, dictionary));
+    }
+
+    private static boolean segmentedString(String lookup, HashSet<String> dictionary) {
+
+        for (int i = 0; i < lookup.length(); i++) {
+            String subSet = lookup.substring(0, i);
+            if (dictionary.contains(subSet)) {
+                String subSet2 = lookup.substring(i);
+
+                if (subSet2 == null || subSet2.length() == 0 || dictionary.contains(subSet2) ||
+                        segmentedString(subSet2, dictionary)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
