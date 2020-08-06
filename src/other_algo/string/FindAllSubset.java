a7c45cdb551cfb6c5e01c9e68fdd6034f6e3192b
Rohit Bhatia <ROHIT.BHATIA@VIZIENTINC.COM>
2020-08-06 00:19:12 -0500

    find all subset

--- src/other_algo/string/FindAllSubset.java
+++ src/other_algo/string/FindAllSubset.java
@@ -1,8 +1,6 @@
 package other_algo.string;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 /*
@@ -15,46 +13,20 @@ import java.util.List;
 public class FindAllSubset {
 
     public static void main(String[] args) {
-
-        Integer[] myints = new Integer[]{2, 3, 4};
-
-        List<Integer> v = Arrays.asList(myints);
-        List<HashSet<Integer>> subsets = new ArrayList<HashSet<Integer>>();
-
-        get_all_subsets(v, subsets);
-
-        System.out.println("****Total*****" + subsets.size());
-        for (int i = 0; i < subsets.size(); ++i) {
-            System.out.print("{");
-            for (Integer it : subsets.get(i)) {
-                System.out.print(it + ", ");
-            }
-            System.out.println("}");
-        }
-        System.out.println("****Total*****" + subsets.size());
+        List<Integer> lst = Arrays.asList(1, 2, 3);
+        int index = 0;
+        String current = "";
+        powerSet(lst, index, current);
     }
 
-    static void get_all_subsets(List<Integer> v, List<HashSet<Integer>> sets) {
-        int subsets_count = (int) (Math.pow((double) 2, (double) v.size()));
-        for (int i = 0; i < subsets_count; ++i) {
-            HashSet<Integer> set = new HashSet<Integer>();
-            for (int j = 0; j < v.size(); ++j) {
-                if (get_bit(i, j) == 1) {
-                    int x = v.get(j);
-                    set.add(x);
-                }
-            }
-            sets.add(set);
+    private static void powerSet(List<Integer> lst, int index, String current) {
+        int len = lst.size();
+        if (index == len) {
+            System.out.println("{" + current + "}");
+            return;
         }
-    }
 
-    static int get_bit(int num, int bit) {
-        int temp = (1 << bit);
-        temp = temp & num;
-        if (temp == 0) {
-            return 0;
-        }
-        return 1;
+        powerSet(lst, index + 1, current + lst.get(index));
+        powerSet(lst, index + 1, current);
     }
-
 }
